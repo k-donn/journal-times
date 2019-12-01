70c8cc20a776af28c51c5c08be5e17aa0bc61a3a
k-donn <mitchell@kevinandjudy.com>
2019-11-30 21:56:22 -0500

    Added accounting for timezones

--- source/graph.py
+++ source/graph.py
@@ -1,10 +1,13 @@
 # TODO
-# 1. Convert UTC times from the JSON into EST dates for Python
+# 1. Format x-axis to be rotated and neat.
+# 2. Format y-axis to be times instead of numbers
+# 3. Apply timezones dynamically instead of all just in NY
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 import datetime
+import pytz
 import json
 
 with open("data/Daily.json", "r") as f:
@@ -12,9 +15,8 @@ with open("data/Daily.json", "r") as f:
 
 raw_dates = [entry["creationDate"] for entry in daily["entries"]]
 
-full_dates = list(map(datetime.datetime.strptime,
-                      raw_dates, len(raw_dates)*['%Y-%m-%dT%H:%M:%SZ']))
-
+full_dates = [datetime.datetime.strptime(
+    raw_date, "%Y-%m-%dT%H:%M:%SZ").astimezone(pytz.timezone("America/New_York")) for raw_date in raw_dates]
 
 # days = mpl.dates.drange(min(full_dates).date(), max(
 #     full_dates).date(), datetime.timedelta(days=1))
@@ -32,11 +34,7 @@ full_dates = list(map(datetime.datetime.strptime,
 days = [mpl.dates.date2num(full_day.date()) for full_day in full_dates]
 y_vals = [(mpl.dates.date2num(full_day) % 1) for full_day in full_dates]
 
-# I'm just plotting points here, but you could just as easily use a bar.
 ax = plt.subplot(111)
 ax.plot_date(days, y_vals, "ro")
 
-# print(hours)
-
 plt.show()
-
